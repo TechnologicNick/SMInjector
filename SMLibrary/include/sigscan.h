f562bde995f55bd3814ad66326bd0b49066ecb73
TechnologicNick <technologicnick@gmail.com>
2021-10-13 22:30:19 +0200

    Add signature scanning

--- SMLibrary/include/sigscan.h
+++ SMLibrary/include/sigscan.h
@@ -16,17 +16,20 @@ private:
 	LPCWSTR moduleName;
 	DWORD64 moduleBase;
 	DWORD moduleSize;
+	HMODULE moduleHandle;
 
 	MODULEINFO GetModuleInfo(LPCWSTR moduleName) {
 		MODULEINFO moduleInfo = { 0 };
-		HMODULE hModule = GetModuleHandle(moduleName);
-		if (hModule == NULL)
+		this->moduleHandle = GetModuleHandle(moduleName);
+		if (this->moduleHandle == NULL)
 			return moduleInfo;
 
-		GetModuleInformation(GetCurrentProcess(), hModule, &moduleInfo, sizeof(MODULEINFO));
+		GetModuleInformation(GetCurrentProcess(), this->moduleHandle, &moduleInfo, sizeof(MODULEINFO));
 
 		return moduleInfo;
 	}
+
+	BYTE* data;
 
 public:
 	SignatureScanner(LPCWSTR moduleName) {
@@ -42,7 +45,46 @@ public:
 		this->moduleBase = (DWORD64) moduleInfo.lpBaseOfDll;
 		this->moduleSize = moduleInfo.SizeOfImage;
 
-		Console::wlog(Color::Aqua, L"[SignatureScanner] Found module %s with base=[0x%llX] size=[%ul]", moduleName, this->moduleBase, this->moduleSize);
+		Console::wlog(Color::Aqua, L"[SignatureScanner] Found module %s with base=[0x%llX] size=[%lu]", moduleName, this->moduleBase, this->moduleSize);
+	}
+
+	~SignatureScanner() {
+		delete[] data;
+	}
+
+	bool readMemory() {
+		data = new BYTE[this->moduleSize];
+
+		if (!ReadProcessMemory(GetCurrentProcess(), (LPCVOID)this->moduleBase, this->data, this->moduleSize, NULL)) {
+#pragma warning(suppress : 4996)
+			Console::log(Color::LightRed, "[SignatureScanner] Failed reading process memory: %s", strerror(errno));
+			return false;
+		}
+
+		return true;
+	}
+
+	DWORD64 scan(const char* signature, const char* mask, DWORD64 offset) {
+		size_t length = strlen(mask);
+
+		for (DWORD64 i = 0; i < this->moduleSize - length - 1; i++) {
+
+			bool found = true;
+
+			for (size_t j = 0; j < length; j++) {
+				found &= mask[j] == '?' || signature[j] == *(char*)(this->moduleBase + i + j);
+			}
+
+			if (found) {
+				return this->moduleBase + i + offset;
+			}
+		}
+
+		return NULL;
+	}
+
+	DWORD64 scan(const char* signature, const char* mask) {
+		return this->scan(signature, mask, 0);
 	}
 
 };
