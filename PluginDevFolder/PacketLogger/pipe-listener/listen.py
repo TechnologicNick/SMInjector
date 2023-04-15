2210a838639fc775b8a45c202671db170f992e10
TechnologicNick <technologicnick@gmail.com>
2023-04-15 19:46:02 +0200

    Update pipe-listener to read new PacketHeader structure

--- PluginDevFolder/PacketLogger/pipe-listener/listen.py
+++ PluginDevFolder/PacketLogger/pipe-listener/listen.py
@@ -2,6 +2,7 @@ import time
 import win32pipe, win32file, pywintypes
 from packets.registry import PacketRegistry
 from packets.direction import Direction
+from packets.action import Action
 
 pipeName = "\\\\.\\pipe\\scrapmechanic"
 
@@ -26,16 +27,10 @@ def pipe_client():
             if res == 0:
                 print(f"SetNamedPipeHandleState return code: {res}")
             while True:
-                resp = win32file.ReadFile(handle, 0x80000)
-                # print(f"message: {resp}")
+                (result, data) = win32file.ReadFile(handle, 0x80000)
 
-                data = resp[1]
-                direction = Direction(data[0]).name
-                packet = registry.get_packet(data[1], data[2:])
-                if not packet.hidden:
-                    # hexdump = " ".join(hex(letter)[2:].zfill(2) for letter in data[2:])
-                    print(f"{direction.ljust(8)}\t packet {hex(data[1]).zfill(2)} ({data[1]}): (size={len(data)-2}) {packet.parse_packet()}")
-
+                process_packet(data)
+                
         except pywintypes.error as e:
             if e.args[0] == 2:
                 print("no pipe, trying again in a sec")
@@ -43,6 +38,20 @@ def pipe_client():
             elif e.args[0] == 109:
                 print("broken pipe, bye bye")
                 quit = True
+
+def process_packet(data: bytes):
+    action = Action(data[0])
+    direction = Direction(data[1])
+    size = int.from_bytes(data[2:6], byteorder="little")
+
+    print(f"{action.name} {direction.name} {size}")
+
+    if action == Action.SendMessageToConnection or action == Action.ReceiveMessagesOnPollGroup:
+        packet = registry.get_packet(data[6], data[7:])
+        if not packet.hidden:
+            # hexdump = " ".join(hex(letter)[2:].zfill(2) for letter in data[2:])
+            print(f"{direction.name.ljust(8)}\t packet {hex(data[1]).zfill(2)} ({data[1]}): (size={len(data)-2}) {packet.parse_packet()}")
+
 
 if __name__ == "__main__":
     pipe_client()
---
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/action.py
@@ -0,0 +1,8 @@
+from enum import IntEnum
+
+class Action(IntEnum):
+    Drop = 0
+    SendReliablePacket = 1
+    SendUnreliablePacket = 2
+    SendMessageToConnection = 3
+    ReceiveMessagesOnPollGroup = 4
