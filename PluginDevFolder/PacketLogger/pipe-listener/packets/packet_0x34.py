9564ee70a2b98e3f2a377b970313ba038ab0362c
TechnologicNick <technologicnick@gmail.com>
2023-06-01 21:45:40 +0200

    Add packet 0x34 (52) Lift Level

---
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/packet_0x34.py
@@ -0,0 +1,25 @@
+from packets.packet import Packet
+from construct import Struct, Int32sb, Int32ub
+
+packet_0x34 = Struct(
+    "lift_id" / Int32ub,
+    "level_change" / Int32sb,
+)
+
+class Packet_0x34(Packet):
+    """Lift Level"""
+
+    def __init__(self, id: int, data: bytes, hidden=False):
+        super().__init__(id, data, hidden)
+
+    def parse_packet(self):
+        self.struct = packet_0x34.parse(self.data)
+        return self.struct
+    
+    def modify_packet(self):
+        # self.struct.lift_id = 1
+        # self.struct.level_change *= 2
+        pass
+    
+    def build_packet(self):
+        return packet_0x34.build(self.struct)
