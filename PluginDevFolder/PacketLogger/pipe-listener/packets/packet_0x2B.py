e3de75debefa34d7a0edd70c066dd213f10f8ed8
TechnologicNick <technologicnick@gmail.com>
2023-08-29 17:21:28 +0200

    Add packet 0x2B (43) Lift Delete Creation

---
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/packet_0x2B.py
@@ -0,0 +1,24 @@
+from packets.packet import Packet
+from construct import Struct, Int32ub
+
+packet_0x2B = Struct(
+    "lift_id" / Int32ub,
+)
+
+class Packet_0x2B(Packet):
+    """Lift Delete Creation"""
+
+    def __init__(self, id: int, data: bytes, hidden=False):
+        super().__init__(id, data, hidden)
+
+    def parse_packet(self):
+        self.struct = packet_0x2B.parse(self.data)
+
+        return self.struct
+    
+    def modify_packet(self):
+        # self.struct.lift_id = 522
+        pass
+    
+    def build_packet(self):
+        return packet_0x2B.build(self.struct)
