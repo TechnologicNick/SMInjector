cd4bd83b777feaf36dee19a473698b445115f0c1
TechnologicNick <technologicnick@gmail.com>
2023-09-01 16:34:32 +0200

    Fix registry packet 0x13 order

--- PluginDevFolder/PacketLogger/pipe-listener/packets/registry.py
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/registry.py
@@ -1,10 +1,10 @@
 from packets.packet_0x12 import Packet_0x12
+from packets.packet_0x13 import Packet_0x13
 from packets.packet_0x16 import Packet_0x16
 from packets.packet_0x17 import Packet_0x17
 from packets.packet_0x18 import Packet_0x18
 from packets.packet_0x1E import Packet_0x1E
 from packets.packet_0x1F import Packet_0x1F
-from packets.packet_0x13 import Packet_0x13
 from packets.packet_0x20 import Packet_0x20
 from packets.packet_0x22 import Packet_0x22
 from packets.packet_0x2B import Packet_0x2B
@@ -18,12 +18,12 @@ class PacketRegistry:
     def __init__(self):
         self.packets = {
             0x12: Packet_0x12,
+            0x13: Packet_0x13,
             0x16: Packet_0x16,
             0x17: Packet_0x17,
             0x18: Packet_0x18,
             0x1E: Packet_0x1E,
             0x1F: Packet_0x1F,
-            0x13: Packet_0x13,
             0x20: Packet_0x20,
             0x22: Packet_0x22,
             0x2B: Packet_0x2B,
