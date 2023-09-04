2ef3e1775888db92db658d3df4fb390caf971af9
TechnologicNick <technologicnick@gmail.com>
2023-09-04 20:12:57 +0200

    Add more Add packet 0x19 (25) Lua Remote Procedure Call (WIP) documentation

--- PluginDevFolder/PacketLogger/pipe-listener/packets/construct_utils.py
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/construct_utils.py
@@ -1,3 +1,5 @@
+from construct import Tunnel
+
 def class_to_dict(cls, exclude_keys=["_io"]):
     """Recursively converts class attributes to a dictionary."""
     d = {}
@@ -9,3 +11,37 @@ def class_to_dict(cls, exclude_keys=["_io"]):
             else:
                 d[k] = attr
     return d
+
+class CompressedLZ4Block(Tunnel):
+    r"""
+    Compresses and decompresses underlying stream before processing subcon. When parsing, entire stream is consumed. When building, it puts compressed bytes without marking the end. This construct should be used with :class:`~construct.core.Prefixed` .
+
+    Parsing and building transforms all bytes using LZ4 library. Since data is processed until EOF, it behaves similar to `GreedyBytes`. Size is undefined.
+
+    :param subcon: Construct instance, subcon used for storing the value
+
+    :raises ImportError: needed module could not be imported by ctor
+    :raises StreamError: stream failed when reading until EOF
+
+    Can propagate lz4.block exceptions.
+
+    Example::
+
+        >>> d = Prefixed(VarInt, CompressedLZ4Block(GreedyBytes))
+        >>> d.build(bytes(100))
+        b'\x0fd\x00\x00\x00\x1f\x00\x01\x00KP\x00\x00\x00\x00\x00'
+        >>> len(_)
+        16
+    """
+
+    def __init__(self, subcon, uncompressed_size=0x400000):
+        super().__init__(subcon)
+        import lz4.block
+        self.lib = lz4.block
+        self.uncompressed_size = uncompressed_size
+
+    def _decode(self, data, context, path):
+        return self.lib.decompress(data, uncompressed_size=self.uncompressed_size)
+
+    def _encode(self, data, context, path):
+        return self.lib.compress(data, store_size=False)
--- PluginDevFolder/PacketLogger/pipe-listener/packets/packet_0x19.py
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/packet_0x19.py
@@ -1,9 +1,10 @@
-from enum import Enum
+from enum import Enum, IntEnum
 from uuid import UUID
+
+from packets.construct_utils import CompressedLZ4Block
 from packets.packet import Packet
-from construct import Byte, BytesInteger, Enum as CEnum, GreedyBytes, Hex, PascalString, Prefixed, Struct, Int16ub, Int32sb, Int32ub, Int32ul
+from construct import Byte, BytesInteger, Enum as CEnum, GreedyBytes, GreedyRange, Hex, Prefixed, Struct, Int16ub, Int32ub, Int32ul
 from packets.hexdump import hexdump
-import lz4.block
 
 class Namespace(Enum):
     CLIENT_DATA = UUID("940af9b6-d4bc-4f16-9f68-e1b8d0608908")
@@ -12,16 +13,16 @@ class Namespace(Enum):
     STORAGE = UUID("7aabd00e-cb5f-4e7a-8c88-386bf709b2cf")
 
 class ScriptTypeID(Enum):
-    UNKNOWN_0 = UUID("fa2d34d7-18d2-4cec-a6fd-35662a5870ed")
-    UNKNOWN_1 = UUID("e8bdb0ca-90ff-4f2f-a296-4a2e8ba72de1")
+    NULL_0 = UUID("fa2d34d7-18d2-4cec-a6fd-35662a5870ed")
+    INTERACTABLE_1 = UUID("e8bdb0ca-90ff-4f2f-a296-4a2e8ba72de1")
     TOOL_2 = UUID("b1668ba4-7680-4052-bb08-6af39fe81e57")
-    UNKNOWN_3 = UUID("428c04d4-c474-415f-abbb-c33426892eb8")
+    WORLD_3 = UUID("428c04d4-c474-415f-abbb-c33426892eb8")
     GAME_4 = UUID("d5493d04-035c-4f29-9c46-f3ceeecc9324")
-    UNKNOWN_5 = UUID("70282349-0e5b-46e4-b9b2-608b6561b2ef")
-    UNKNOWN_6 = UUID("04428a85-ee85-44f2-a534-3a63b05dd608")
-    UNKNOWN_7 = UUID("6e4fbfbe-36d3-43de-a16b-08944bb6b0c7")
-    UNKNOWN_8 = UUID("84b266b1-c8f2-4254-bba2-1a5895485ff0")
-    UNKNOWN_9 = UUID("453b3b65-e7fa-43ce-bbe8-82d1a970b87c")
+    CHARACTER_5 = UUID("70282349-0e5b-46e4-b9b2-608b6561b2ef")
+    HARVESTABLE_6 = UUID("04428a85-ee85-44f2-a534-3a63b05dd608")
+    PLAYER_7 = UUID("6e4fbfbe-36d3-43de-a16b-08944bb6b0c7")
+    UNIT_8 = UUID("84b266b1-c8f2-4254-bba2-1a5895485ff0")
+    SCRIPTABLE_OBJECT_9 = UUID("453b3b65-e7fa-43ce-bbe8-82d1a970b87c")
 
 def uuid5(namespace: UUID, name: UUID):
     from hashlib import sha1
@@ -40,14 +41,21 @@ uuidToNamespaceScriptType = {
     getEnumName(namespace, scriptTypeID): int.from_bytes(uuid5(namespace.value, scriptTypeID.value).bytes) for namespace in Namespace for scriptTypeID in ScriptTypeID
 }
 
-packet_0x19 = Struct(
-    "tick" / Int32ub,
+class RpcType(IntEnum):
+    CALLBACK = 0x14
+
+rpc = Struct(
     "uuid" / CEnum(BytesInteger(16), **uuidToNamespaceScriptType),
-    "type" / Int16ub,
+    "unknown" / Int16ub,
     "id" / Int32ul,
     "world_id" / Int16ub,
-    "padding" / Hex(BytesInteger(1)),
-    "data" / Prefixed(Int32ub, GreedyBytes)
+    "type" / CEnum(Hex(Byte), RpcType),
+    "data" / Prefixed(Int32ub, CompressedLZ4Block(GreedyBytes)),
+)
+
+packet_0x19 = Struct(
+    "tick" / Int32ub,
+    "rpcs" / GreedyRange(rpc),
 )
 
 class Packet_0x19(Packet):
@@ -62,8 +70,8 @@ class Packet_0x19(Packet):
 
         print(self.struct)
 
-        # self.data = lz4.block.decompress(self.struct.data, uncompressed_size=1000)
-        # print(self.data)
+        for rpc in self.struct.rpcs:
+            print(hexdump(rpc.data))
 
         return hexdump(self.data)
     
@@ -71,5 +79,4 @@ class Packet_0x19(Packet):
         pass
     
     def build_packet(self):
-        return self.data
-        # return packet_0x19.build(self.struct)
+        return packet_0x19.build(self.struct)
