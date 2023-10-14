780af4ee8f00c09928e2e3cfcba45cb95d764d2b
TechnologicNick <technologicnick@gmail.com>
2023-10-14 16:52:55 +0200

    Rewrite packet 0x1E (30) Player Movement

--- PluginDevFolder/PacketLogger/pipe-listener/packets/packet_0x1E.py
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/packet_0x1E.py
@@ -1,23 +1,36 @@
+from construct import Aligned, BitsInteger, Bitwise, Byte, Const, Flag, Int32ub, Struct
 from packets.packet import Packet
-from packets.bitfield import Bitfield
+
+packet_0x1E = Struct(
+    "tick" / Int32ub,
+    "pressed_keys" / Bitwise(Struct(
+        Const(0, BitsInteger(4)),
+        "sprint" / Flag,
+        "horizontal" / Flag,
+        "crawl" / Flag,
+        "jump" / Flag,
+    )),
+    "direction" / Byte,
+    "yaw" / Byte, # 0 = right, increases by 64 every 45 degrees counter-clockwise
+    "pitch" / Byte, # 0 is down, 128 is up
+)
 
 class Packet_0x1E(Packet):
-    def __init__(self, id: int, data: bytes, hidden=True):
+    """Player Movement"""
+
+    def __init__(self, id: int, data: bytes, hidden=False):
         super().__init__(id, data, hidden)
 
     def parse_packet(self):
-        keys = Bitfield(self.data[5])
-        direction = Bitfield(self.data[6])
-        return {
-            "tick": int.from_bytes(self.data[1:5], byteorder="big"),
-            "keys": {
-                "jump": keys.get_bit(0),
-                "crouch": keys.get_bit(1),
-                "wasd": keys.get_bit(2),
-                "sprint": keys.get_bit(3),
-            },
-            "direction": direction.get_int_from_bits([7, 6, 5]), # 0 = right, increases by 1 every 45 degrees counter-clockwise
-            "yaw": self.data[7],
-            "pitch": self.data[8],
-        }
+        self.struct = packet_0x1E.parse(self.data)
+        return self.struct
+
+    def modify_packet(self, direction):
+        # self.struct.direction = (self.struct.direction + 64) % 256
+        # self.struct.yaw = (self.struct.yaw - 64) % 256
+        # self.struct.pressed_keys.sprint = True
+        pass
     
+    def build_packet(self):
+        # return self.data
+        return packet_0x1E.build(self.struct)
