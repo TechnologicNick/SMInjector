789be9ad0095e44f2518a251136cc217d9428c51
TechnologicNick <technologicnick@gmail.com>
2024-03-10 00:07:21 +0100

    Add packet 0x03 (3) Request Passphrase

--- PluginDevFolder/PacketLogger/pipe-listener/packets/registry.py
+++ PluginDevFolder/PacketLogger/pipe-listener/packets/registry.py
@@ -1,4 +1,5 @@
 from packets.packet_0x01 import Packet_0x01
+from packets.packet_0x03 import Packet_0x03
 from packets.packet_0x06 import Packet_0x06
 from packets.packet_0x12 import Packet_0x12
 from packets.packet_0x13 import Packet_0x13
@@ -25,6 +26,7 @@ class PacketRegistry:
     def __init__(self):
         self.packets = {
             0x01: Packet_0x01,
+            0x03: Packet_0x03,
             0x06: Packet_0x06,
             0x12: Packet_0x12,
             0x13: Packet_0x13,
